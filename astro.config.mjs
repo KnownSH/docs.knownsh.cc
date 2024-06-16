a6ec88ba36ede669a9d98b9e9bbc1d95203e1327
KnownSH <xanp101@gmail.com>
2024-06-15 18:08:18 -0700

    ModCard

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 import starlightImageZoom from 'starlight-image-zoom';
 import vercel from '@astrojs/vercel/serverless';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
@@ -12,23 +14,33 @@ export default defineConfig({
     social: {},
     customCss: ['./src/styles/custom.css', './src/styles/tailwind.css'],
     expressiveCode: {
-      styleOverrides: { 
-        borderRadius: '0.5rem',
+      styleOverrides: {
+        borderRadius: '0.5rem'
       },
       themes: ['dark-plus', 'github-light']
     },
     sidebar: [{
-      label: 'Guides',
+      label: 'FiguraSVC',
       items: [
       // Each item here is one entry in the navigation menu.
       {
-        label: 'FiguraSVC 1.1.0',
+        label: '2.0',
+        link: '/guides/figurasvc2/'
+      }, {
+        label: '1.1',
         link: '/guides/figurasvc/'
+      }, {
+        label: '1.0',
+        link: '/guides/figurasvc0/'
       }]
     }]
   }), tailwind({
     applyBaseStyles: false
-  })],
+  }), react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
